5ab2efd13afdd0cdfb40b5449e2c2c27a2e73e93
Duncan Moyo <duncanfmoyo@gmail.com>
2020-09-07 18:26:46 +0200

    SERVER hashing passwords

--- server/models/User.js
+++ server/models/User.js
@@ -1,44 +1,64 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    unique: 1
+    unique: 1,
   },
   password: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
   name: {
     type: String,
     required: true,
-    maxlength: 25
+    maxlength: 25,
   },
   lastName: {
     type: String,
     required: true,
-    maxlength: 25
+    maxlength: 25,
   },
   cart: {
     type: Array,
-    default: []
+    default: [],
   },
   history: {
     type: [],
-    default: []
+    default: [],
   },
   role: {
     type: Number,
-    default: 0
+    default: 0,
   },
   token: {
-    type: String
+    type: String,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    bcrypt.genSalt(SALT_I, function (err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(error);
+
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
   }
-})
+});
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = {User}
+module.exports = { User };
