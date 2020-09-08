7b1a87ec3eef142a4839d4c080f803f0c22201b7
Duncan Moyo <duncanfmoyo@gmail.com>
2020-09-09 00:33:35 +0200

    SERVER Brand model and routes

--- server/Server.js
+++ server/Server.js
@@ -14,11 +14,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+
 // Models
 const { User } = require("./models/User");
+const {Brand} = require("./models/Brand")
+
 
 // Middlewares
 const {auth} = require('./Middleware/Auth')
+const {admin} = require('./Middleware/Admin')
+
+//==========================
+//         BRAND
+//==========================
+
+app.post('/api/product/brand', auth, admin, (req, res) => {
+  const brand = new Brand(req.body)
+
+  brand.save((err, doc) => {
+    if(err) return res.json({success: false, err})
+    res.status(200).json({
+      success: true,
+      brand: doc
+    })
+  })
+})
+
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if(err) return res.status(400).send(err)
+    res.status(200).send(brands)
+  })
+})
 
 //==========================
 //         USERS
