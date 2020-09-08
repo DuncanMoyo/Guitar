6fbac6d88300e62e92c333de6b6df965fca69086
Duncan Moyo <duncanfmoyo@gmail.com>
2020-09-08 21:04:50 +0200

    SERVER Login users and creating tokens

--- server/Server.js
+++ server/Server.js
@@ -33,6 +33,28 @@ app.post("/api/users/register", (req, res) => {
   });
 });
 
+app.post("/api/users/login", (req, res) => {
+  User.findOne({ 'email': req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+
+    user.comparePassword({'password': req.body.password}, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+
+      user.generateToken((err, user) => {
+        if (err) return res.status(400).send(err);
+        res.cookie("x_auth", user.token).status(200).json({
+          loginSuccess: true,
+        });
+      });
+    });
+  });
+});
+
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Server Running at ${port}`);
