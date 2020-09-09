92b6ebfdc47765c7df35411c3c861d9866e0504c
Duncan Moyo <duncanfmoyo@gmail.com>
2020-09-09 23:26:50 +0200

    SERVER Adding Products

--- server/Server.js
+++ server/Server.js
@@ -14,67 +14,81 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-
 // Models
 const { User } = require("./models/User");
-const {Brand} = require("./models/Brand")
-const {Wood} = require("./models/Wood")
-
+const { Brand } = require("./models/Brand");
+const { Wood } = require("./models/Wood");
+const { Product } = require("./models/Product");
 
 // Middlewares
-const {auth} = require('./Middleware/Auth')
-const {admin} = require('./Middleware/Admin')
+const { auth } = require("./Middleware/Auth");
+const { admin } = require("./Middleware/Admin");
 
+//==========================
+//         PRODUCTS
+//==========================
+
+app.post("/api/product/article", auth, admin, (req, res) => {
+  const product = new Product(req.body);
+  product.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      article: doc,
+    });
+  });
+});
+  
 //==========================
 //         WOODS
 //==========================
 
-app.post('/api/product/wood', auth, admin, (req, res) => {
-  const wood = new Wood(req.body)
+app.post("/api/product/wood", auth, admin, (req, res) => {
+  const wood = new Wood(req.body);
   wood.save((err, doc) => {
-    if(err) return res.json({success: false, err})
+    if (err) return res.json({ success: false, err });
     res.status(200).json({
-      success: true, 
-      wood: doc      
-    })
-  })
-})
+      success: true,
+      wood: doc,
+    });
+  });
+});
 
-app.get('/api/product/woods', (req, res) => {
+app.get("/api/product/woods", (req, res) => {
   Wood.find({}, (err, woods) => {
-    if(err) return res.status(400).send(err)
-    res.status(200).send(woods)
-  })
-})
+    if (err) return res.status(400).send(err);
+    res.status(200).send(woods);
+  });
+});
 
 //==========================
 //         BRAND
 //==========================
 
-app.post('/api/product/brand', auth, admin, (req, res) => {
-  const brand = new Brand(req.body)
+app.post("/api/product/brand", auth, admin, (req, res) => {
+  const brand = new Brand(req.body);
 
   brand.save((err, doc) => {
-    if(err) return res.json({success: false, err})
+    if (err) return res.json({ success: false, err });
     res.status(200).json({
       success: true,
-      brand: doc
-    })
-  })
-})
+      brand: doc,
+    });
+  });
+});
 
-app.get('/api/product/brands', (req, res) => {
+app.get("/api/product/brands", (req, res) => {
   Brand.find({}, (err, brands) => {
-    if(err) return res.status(400).send(err)
-    res.status(200).send(brands)
-  })
-})
+    if (err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  });
+});
 
 //==========================
 //         USERS
 //==========================
 
-app.get('/api/users/auth', auth, (req, res) => {
+app.get("/api/users/auth", auth, (req, res) => {
   res.status(200).json({
     isAdmin: req.user.role === 0 ? false : true,
     isAuth: true,
@@ -83,9 +97,9 @@ app.get('/api/users/auth', auth, (req, res) => {
     lastName: req.user.lastName,
     role: req.user.role,
     cart: req.user.cart,
-    history: req.user.history
-  })
-})
+    history: req.user.history,
+  });
+});
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
@@ -100,14 +114,14 @@ app.post("/api/users/register", (req, res) => {
 });
 
 app.post("/api/users/login", (req, res) => {
-  User.findOne({ 'email': req.body.email }, (err, user) => {
+  User.findOne({ email: req.body.email }, (err, user) => {
     if (!user)
       return res.json({
         loginSuccess: false,
         message: "Auth failed, email not found",
       });
 
-    user.comparePassword({'password': req.body.password}, (err, isMatch) => {
+    user.comparePassword({ password: req.body.password }, (err, isMatch) => {
       if (!isMatch)
         return res.json({ loginSuccess: false, message: "Wrong password" });
 
@@ -121,20 +135,14 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
-app.get('/api/user/logout', auth, (req, res) => {
-  User.findOneAndUpdate(
-    {_id: req.user._id},
-    {token: ''},
-    (err, doc) => {
-      if(err) return res.json({success: false, err})
-      return res.status(200).send({
-        success: true
-      })
-    }
-
-  )
-})
-
+app.get("/api/user/logout", auth, (req, res) => {
+  User.findOneAndUpdate({ _id: req.user._id }, { token: "" }, (err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).send({
+      success: true,
+    });
+  });
+});
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
