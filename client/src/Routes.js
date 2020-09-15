db7b912a0a10a7ab82b4b79dfa77a80e5481021f
Duncan Moyo <duncanfmoyo@gmail.com>
2020-09-15 23:03:34 +0200

    CLIENT Creating the dashboard

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -4,12 +4,16 @@ import Layout from "./components/hoc/Layout";
 import Home from "./components/Home";
 import RegisterLogin from "./components/Register_Login";
 import Register from "./components/Register_Login/Register";
+import UserDashboard from "./components/User";
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
-      <Route path="/" exact component={Home} />
+
+        <Route path="/user/dashboard" exact component={UserDashboard} />
+
+        <Route path="/" exact component={Home} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/register" exact component={Register} />
       </Switch>
---
+++ client/src/components/User/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import UserLayout from "../hoc/UserLayout";
+import MyButton from "../utils/Button";
+
+const UserDashboard = () => {
+  return (
+    <UserLayout>
+      <div>
+        <div className="user_nfo_panel">
+          <h1>User Information</h1>
+          <div>
+            <span>Name</span>
+            <span>Last Name</span>
+            <span>Email</span>
+          </div>
+          <MyButton
+            type="default"
+            title="Edit Account Info"
+            linkTo="/user/user_profile"
+          />
+        </div>
+        <div className='user_nfo_panel'>
+          <h1>History Purchases</h1>
+          <div className='user_product_block_wrapper'>
+            History
+          </div>
+        </div>
+      </div>
+    </UserLayout>
+  );
+};
+
+export default UserDashboard;
