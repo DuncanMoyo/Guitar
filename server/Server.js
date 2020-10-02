499819dc45a48135e5b073dc05be6b53275906ef
Duncan Moyo <duncanfmoyo@gmail.com>
2020-10-02 16:25:47 +0200

    ADMIN Finishing the Buy Success Route

--- server/Server.js
+++ server/Server.js
@@ -8,6 +8,7 @@ const app = express();
 const mongoose = require("mongoose");
 const { mongo } = require("mongoose");
 require("dotenv").config();
+const async = require('async')
 
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.DATABASE);
@@ -365,6 +366,43 @@ app.post('/api/users/successBuy', auth, (req, res) => {
   }
   transactionData.data = req.body.paymentData
   transactionData.product = history
+
+  User.findOneAndUpdate(
+    {_id: req.user._id},
+    {$push : {history: history}, $set: {cart: []}},
+    {new: true},
+    (err, user) => {
+      if(err) return res.json({success: false, err})
+
+      const payment = new Payment(transactionData)
+      payment.save((err, doc) => {
+        if(err) return res.json({success: false, err})
+
+        let products = []
+        doc.product.forEach(item => {
+          products.push({id: item.id, quantity: item.quantity})
+        })
+
+        async.eachOfSeries(products, (item, callback) => {
+          Product.update(
+            {_id: item.id}, 
+            {$inc: {
+              'sold': item.quantity
+            }},
+            {new: false},
+            callback
+          )
+        }, (err) => {
+          if(err) return res.json({success: false, err})
+          res.status(200).json({
+            success: true,
+            cart: user.cart,
+            cartDetail: []
+          })
+        })
+      })
+    }
+  )
 })
 
 const port = process.env.PORT || 3002;
